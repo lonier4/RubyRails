b6d229453106568257813666e7fffa0098b7f6fc
Andre Yeargins <lonier4@gmail.com>
2019-09-07 19:53:31 +0000

    Deleted new.html.erb file under quotes, moved simple_form from controller file to application.html.erb which is file that will show on all pages. Used bootstrap modal in Contribute

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,26 +2,21 @@ class QuotesController < ApplicationController
   def index
     @quote = Quote.order("Random()").first
   end
-
-
-  def new
-    @quote = Quote.new
-  end
-
+  
   def create
     @quote = Quote.create(quote_params)
     if @quote.invalid?
-    flash[:error] = '<strong>Whoops!Could not save</strong> the data you entered is invalid.'
+      flash[:error] = '<strong>Whoops!Could not save</strong> the data you entered is invalid.'
+    end
+    redirect_to root_path
   end
-  redirect_to root_path
-end
 
-def about
+  def about
   end
 
   private
 
   def quote_params
     params.require(:quote).permit(:saying, :author)
-end
+  end
 end
