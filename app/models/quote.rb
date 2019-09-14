4ee13015d2f51864c0732f5550a221c13d8e574b
lonier4 <53928990+lonier4@users.noreply.github.com>
2019-09-14 17:30:01 -0500

    ts

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,5 @@
 class Quote < ApplicationRecord
-  validates :saying, presence: true, length: { maximum: 140, minimum: 3 }, format: { with: /\A[a-zA-Z]+\z/,
-    message: "only allows letters" }
+  validates :saying, presence: true, length: { maximum: 140, minimum: 3 }
     validates :author, presence: true, length: { maximum: 50, minimum: 3 }
 
 end
